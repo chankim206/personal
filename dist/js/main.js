40446a277e5cdc92f642f7c985c605e904d3256f
Young-Chan Kim <chankim206@gmail.com>
2019-11-29 20:47:32 -0800

    doing menu button css

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,28 +6,14 @@ const menuBrand = document.querySelector(".menu-branding");
 
 const navItems = document.querySelectorAll(".nav-item");
 
-let show = false;
-
 menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-  if (!show) {
-    menuBtn.classList.add("close");
-    menu.classList.add("show");
-    menuNav.classList.add("show");
-    menuBrand.classList.add("show");
-    navItems.forEach(item => {
-      item.classList.add("show");
-    });
-    show = true;
-  } else {
-    menuBtn.classList.remove("close");
-    menu.classList.remove("show");
-    menuNav.classList.remove("show");
-    menuBrand.classList.remove("show");
-    navItems.forEach(item => {
-      item.classList.remove("show");
-    });
-    show = false;
-  }
+  menuBtn.classList.toggle("close");
+  menu.classList.toggle("show");
+  menuNav.classList.toggle("show");
+  menuBrand.classList.toggle("show");
+  navItems.forEach(item => {
+    item.classList.toggle("show");
+  });
 }
